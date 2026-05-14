ad257440e5c3348af4e72057f1b7facafed1e7d2
Rajapurv <rajapurv231@gmail.com>
2026-05-14 10:41:56 +0530

    update

---
+++ src/main/java/apoorv/practice/Book.java
@@ -0,0 +1,23 @@
+package apoorv.practice;
+
+public class Book {
+    private String title,author;
+    private float price;
+    public void setBook(String title,String author,float price){
+        this.author = author;
+        this.title = title;
+        this.price = price;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public float getPrice(){
+        return price;
+    }
+    public void applyDiscount(float percent){
+        price = price * percent / 100;
+    }
+}
