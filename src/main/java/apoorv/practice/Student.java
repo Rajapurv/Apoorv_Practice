14384ca1b7c201ff07451cd0240c0811ff1ffab2
Rajapurv <rajapurv231@gmail.com>
2026-05-14 17:16:23 +0530

    update

--- src/main/java/apoorv/practice/Student.java
+++ src/main/java/apoorv/practice/Student.java
@@ -3,11 +3,20 @@ package apoorv.practice;
 public class Student {
     private int student_id,grades;
     private String student_name;
-    public void Student(String student_name,int student_id){
+    public Student(String student_name,int student_id){
         this.student_id = student_id;
         this.student_name = student_name;
     }
     public void addGrade(int grades){
         this.grades = this.grades + grades;
     }
+    public Student(){};
+    public Student(int student_id, int grades){
+        this.student_id = student_id;
+        this.grades = grades;
+    }
+    public void printData(){
+        System.out.println(this.student_id +" "+ this.student_name);
+    }
+
 }
